da9f57715cb3102bbd3615d2e14bdf0c43c57a3b
Richard Li <richardli@richards-air.home>
2019-08-06 03:06:45 +0200

    added self-committing data to prototype, fixed plant balance data

--- prototype.py
+++ prototype.py
@@ -7,6 +7,7 @@ This is a temporary script file.
 
 import os
 import pandas as pd
+from fuzzywuzzy import fuzz, process
 
 ## This script generates a prototype database with the following attributes
 ## for each coal plant:
@@ -26,38 +27,74 @@ plant_cols = ['Plant Name', 'Plant Code', 'Utility Name',
 plants = pd.read_csv(os.path.join(cwd, 'eia860', '3_1_Generator_Y2017.csv'), skiprows=1, usecols=plant_cols)
 owners = pd.read_csv(os.path.join(cwd, 'eia860', '4___Owner_Y2017.csv'), skiprows=1)
 
-# import depreciation model outputs (NEEDS TO BE UPDATED)
-balance_or = pd.read_csv(os.path.join(cwd, 'plant balance [wrong].csv'), skiprows=1)
-balance = balance_or[balance_or['EIA Plant ID Best Match'].notnull()]
-balance = balance[balance['Account Title'].notnull()]
+# selecting only coal plants for now
+#plants2 = plants[plants['Technology'] == 'Conventional Steam Coal']
+plants2=plants[plants['Plant Name'].notnull()]
 
-# matching plant and unit between depreciation model and EIA 860
-plants['Generator ID'] = plants['Generator ID'].astype(str)
+# import depreciation model outputs
+balance_or = pd.read_csv(os.path.join(cwd, 'plant balance.csv'))
+balance = balance_or[balance_or['Current Net Plant Balance Incl. Removal Net of Salvage ($)'].notnull()
+ & balance_or['Current Net Plant Balance Incl. Removal Net of Salvage ($)'] != 0]
+
+unit_list = pd.read_csv(os.path.join(cwd, 'master unit list.csv'), skiprows=2)
+unit_list = unit_list[unit_list['Plant Code'].notnull()]
+unit_list['Plant Code'] = unit_list['Plant Code'].astype(int).astype(str)
+
+# matching plant & unit between depreciation model and EIA 860
+plant_names = sorted(list(set(plants2['Plant Name'])))
 for i, row in balance.iterrows():
-    if row['EIA Plant ID Best Match'] in set(plants['Plant Code']):
-        matches = plants[plants['Plant Code'] == row['EIA Plant ID Best Match']]  
-        if row['Account Title'][-1].isdigit():
-            matches = matches[matches['Generator ID'] == row['Account Title'][-1]]
-            if(not matches.empty):
-                plants.loc[matches.index[0], 'Plant Balance'] = row['ANNUAL AMOUNT']
-        else:
-            plants.loc[matches.index[0], 'Plant Balance'] = row['ANNUAL AMOUNT']
-            
-# selecting only coal plants with a plant balance for now
-plants2 = plants[plants['Plant Balance'].notnull()]
-plants2 = plants2[plants2['Technology'] == 'Conventional Steam Coal']
+    if '_' in row['Plant ID']:
+        match = unit_list[unit_list['Plant Generator ID'] == row['Plant ID']].iloc[0]
+        eia_name = process.extractOne(match['Plant Name'], plant_names)[0]
+        plants2.loc[(plants2['Plant Name'] == eia_name) & 
+                    (plants2['Generator ID'] == match['Generator ID']), 
+                    'Plant Balance'] = row['Current Net Plant Balance Incl. Removal Net of Salvage ($)']
+        
+    else:
+        match = unit_list[unit_list['Plant Code'] == row['Plant ID']].iloc[0]
+        eia_name = process.extractOne(match['Plant Name'], plant_names)[0]
+        plants2.loc[plants2['Plant Name'] == eia_name, 
+                    'Plant Balance'] = row['Current Net Plant Balance Incl. Removal Net of Salvage ($)']
+
+# select only coal plants or those with plant balances
+plants2 = plants2[(plants2['Technology'] == 'Conventional Steam Coal') | 
+                 (plants2['Plant Balance'].notnull())]
+
 
 # adding multiple ownership information
 for i, row in plants2[plants2['Ownership'] != 'S'].iterrows():
     own = owners[(owners['Plant Code'] == row['Plant Code']) & 
                  (owners['Generator ID'] == row['Generator ID'])]
     own.reset_index(inplace=True)
+    count = 1
     for j, r in own.iterrows():
-        plants2.loc[i, 'Owner #' + str(j)] = r['Owner Name']
-        plants2.loc[i, '% Owned #' + str(j)] = r['Percent Owned']
-
-#plants2.to_csv('/Users/richardli/Documents/Coal Database/prototype output.csv')
-
-
+        if r['Percent Owned'] > .1:
+            plants2.loc[i, 'Owner #' + str(count)] = r['Owner Name']
+            plants2.loc[i, '% Owned #' + str(count)] = r['Percent Owned']
+            count += 1
 
 
+## ADD SELF COMMITTING DATA
+sc = pd.read_csv(os.path.join(cwd, 'self_committing.csv'))
+
+# fuzzy matching for plant names between self-committing dataset and EIA.
+plant_names = sorted(list(set(plants2['Plant Name'])))
+name_mapping = {}
+for i, row in sc.iterrows():
+    name_mapping[row['Plant Name']] = process.extractOne(row['Plant Name'], plant_names, scorer = fuzz.partial_ratio)[0]
+
+# the top match from above was chosen for each except for the following exceptions,
+# which were manually matched:
+name_mapping["Scottsbluff ST Plant"] = "Western Sugar Coop - Scottsbluff"
+name_mapping["Grand River Energy Center (GRDA)"] = "GREC"
+
+# add self-committing data to main dataset
+for i, row in sc.iterrows():
+    plants2.loc[(plants2['Plant Name'] == name_mapping[row['Plant Name']]) & 
+                (plants2['Generator ID'] == row['Unit No']), "Profits"] = \
+    row['2-year Cumulative Profits (Losses) ($ Millions)']
+    plants2.loc[(plants2['Plant Name'] == name_mapping[row['Plant Name']]) & 
+                (plants2['Generator ID'] == row['Unit No']), "CF Offset"] = \
+    row['Expected vs Actual']
+                
+    
