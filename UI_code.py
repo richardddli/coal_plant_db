7d88ef43b3744e8eeeb49babe16dfd599b93d875
Sam Mardell <smardell@MBA-5EE-014-L18.local>
2019-09-04 08:41:40 -0600

    Tied UI_Code.py to prototype.py

--- UI_code.py
+++ UI_code.py
@@ -4,76 +4,87 @@ import numpy as np
 import matplotlib.pyplot as plt
 import xlwings as xw
 from xlwings import Range
-import partial_ownership
-import prototype
+import prototype as pt
 
-#universal variables
-ui = xw.Book.sheets['Sheet3'] #connects to xlsm macro
+def create_csv():
+    sht = xw.Book.caller().sheets['inputs'] #for calling parameters
+    pst = xw.Book.caller().sheets['csv'] #for pasting results
 
-filter_states = ui.Range('F11').value #reads in selected utility
-filter_markets = ui.Range('F13').value #reads in selected utility
-filter_utilities = ui.Range('F20').value #reads in selected utility
-filter_utiltypes = ui.Range('F22').value #reads in selected utility
-filter_sierra = ui.Range('F29').value #reads in selected utility
+    #data subsetting selections
+    State = Range('F11').value  # reads in selected utility
+    #Market = sht.Range('F13').value  # reads in selected utility
+    Utility = Range('F20').value  # reads in selected utility
+    #Utility_type = sht.Range('F22').value  # reads in selected utility
+    Sierra = Range('F29').value  # reads in selected utility
 
-data_aggregation = ui.Range('N11').value #reads in selected utility
-data_sort = ui.Range('N18').value #reads in selected utility
+    #data output selections
+    Level = Range('N11').value  # reads in selected utility
+    Sort = Range('N18').value  # reads in selected utility
+    #file_path = sht.range('N25').value  # reads in selected utility
 
-file_path = ui.Range('N25').value #reads in selected utility
+    #define df depending on analysis type
+    df = pt.plants
 
-def capacity():
-    # read in partial ownership series, turn series into a dataframe
-    file = pd.Series(partial_ownership.total_MW)  # read in series with utilities and mw values
-    file = file.to_frame()
-    file.rename(columns={0: 'Capacity (MW)'}, inplace=True)
+    #analysis functions
+    if State:
+        newdf = pt.select_by_attribute(df, 'State', State)
+    elif Utility:
+        newdf = pt.select_by_attribute(df, 'Utility Name', Utility)
+    else:
+        newdf = df
 
-    xw.Book.caller() #connects to xlsm macro
-    utility = Range('C2').value #reads in selected utility
-    Range('C3').value = file.get_value(utility, 'Capacity (MW)') #returns value in xlsm ui
+    if Sierra:
+        newdf = pt.select_by_attribute(newdf, 'Current Designation', Sierra)
+    else:
+        newdf = newdf
+
+    if Level:
+        newdf = pt.aggregate_by_level(newdf, Level)
+    else:
+        newdf = newdf
+
+    if Sort:
+        newdf = pt.sort_by_attribute(newdf, 'Nameplate Capacity (MW)', ascending=False)
+    else:
+        newdf = newdf
+
+    #use file_path to create new csv file with data
+    xw.view(newdf, sheet=pst)
 
 if __name__ == '__main__':
     # Expects the Excel file next to this source file, adjust accordingly.
     xw.Book('UI_Mockup.xlsm').set_mock_caller()
-    HelloWorld()
+    CreateCsv()
 
-def get_figure():
-    # read in prototype df
-    main = prototype.plants2
-
-    main.set_index('Plant Name', drop=True, inplace=True)
-    largest = main.nlargest(10, 'Plant Balance')
-    ax = largest['Plant Balance'].plot(kind='bar', figsize=(3,3), title='Top 10 Plant Balances')
-    fig = ax.get_figure()
-    return fig
-
-def plot_cap():
-    # read in prototype df
-    main = prototype.plants2
-    pb = prototype.pb
+def create_chart():
+    # read in prototype df and python visualizations
+    df = pt.plants
+    pb = pt.pb
+    cf = pt.cf
+    pr = pt.pr
 
     # Create a reference to the calling Excel Workbook
-    sht = xw.Book.caller().sheets['Sheet1'] #connects to xlsm macro
+    sht = xw.Book.caller().sheets['inputs'] #for calling parameters
+    pst = xw.Book.caller().sheets['vis'] #for pasting results
 
-    if Range('B6').value == 'Python':
-        #reate visualization in python
-        #fig = get_figure()
-        fig = pb
-        sht.pictures.add(fig, name='MyPlot', update=True, left=sht.range('E2').left, top=sht.range('E2').top)
+    chart = Range('V11').value
+
+    if Range('V21').value == 'Python':
+        # create visualization in python
+        if chart == 'Plant Balance':
+            fig = pb
+        elif chart == 'CF Offset':
+            fig = cf
+        elif chart == 'Profits':
+            fig = pr
+        pst.pictures.add(fig, name='MyPlot', update=True, left=sht.range('E2').left, top=sht.range('E2').top)
     else:
-        # Get dataframe selection and show it in Excel
-        main.sort_values(by=['Plant Balance'])
-        largest = main.nlargest(10, 'Plant Balance')
-        largest = largest[['Utility Name', 'Plant Name', 'Plant Balance']]
-        sht.range('A12').value = largest
+        df.sort_values(by=[chart])
+        largest = df.nlargest(10, chart)
+        largest = largest[['Utility Name', 'Plant Name', chart]]
+        pst.range('A1').options(index=False).value = largest
 
-#def csv_out()
-    #read in data
-    #read in inputs
-    #Slice, dice, ect
-    #paste output to chosen destination
-
-#def chart_out()
-    #read in data
-    #read in inputs
-    #Slice, dice, ect
-    #paste output to chosen destination
+if __name__ == '__main__':
+    # Expects the Excel file next to this source file, adjust accordingly.
+    xw.Book('UI_Mockup.xlsm').set_mock_caller()
+    CreateChart()
